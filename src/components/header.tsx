be290924b69bf7925c6cb78cc3bc73ac8a6f4490
angelDova <Angelfcb01@yahoo.com>
2024-03-18 16:22:36 -0700

    first commit

---
+++ src/components/header.tsx
@@ -0,0 +1,13 @@
+import WordMark from "@/components/word-mark";
+import { Navbar } from "@/components/navbar";
+import { createClient } from "@/prismicio";
+
+export const Header = async () => {
+  const client = createClient();
+  const settings = await client.getSingle("settings");
+  return (
+    <header>
+      <Navbar settings={settings} />
+    </header>
+  );
+};

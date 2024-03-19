438e6e0aca322e1f97d220ec2c412a8933de06b6
angelDova <Angelfcb01@yahoo.com>
2024-03-19 15:19:21 -0700

    gsap animations

--- src/slices/Integrations/index.tsx
+++ src/slices/Integrations/index.tsx
@@ -19,6 +19,7 @@ import {
 } from "react-icons/fa6";
 import React from "react";
 import clsx from "clsx";
+import AnimatedContent from "./animated-content";
 
 /**
  * Props for `Integrations`.
@@ -62,32 +63,7 @@ const Integrations = ({ slice }: IntegrationsProps): JSX.Element => {
           <PrismicRichText field={slice.primary.body} />
         </div>
 
-        <div className="mt-20 flex flex-col items-center md:flex-row">
-          {slice.items.map((item, index) => (
-            <React.Fragment key={index}>
-              {index === Math.floor(slice.items.length / 2) && (
-                <>
-                  <StylizedLogoMark />
-                  <div className="signal-line rotate-180 bg-gradient-to-t" />
-                </>
-              )}
-              <div className="pulsing-icon flex aspect-square shrink-0 items-center justify-center rounded-full border border-blue-50/30 bg-blue-50/25 p-3 text-3xl text-blue-100 opacity-40 md:text-4xl lg:text-5xl">
-                {item.icon && icons[item.icon]}
-              </div>
-              {index !== slice.items.length - 1 && (
-                <div
-                  className={clsx(
-                    "signal-line",
-                    index >= Math.floor(slice.items.length / 2)
-                      ? "rotate-180"
-                      : "rotate-0",
-                  )}
-                />
-              )}
-            </React.Fragment>
-          ))}
-        </div>
-        {/* <AnimatedContent slice={slice} /> */}
+        <AnimatedContent slice={slice} />
       </div>
     </Bounded>
   );
